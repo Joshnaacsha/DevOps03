111ecc8fddfb2c5833778b80acb98cf8304a4ba3
Joshna Acsha S <joshnaacsha@gmail.com>
2024-03-27 14:27:41 +0530

    First Commit

---
+++ src/main/java/com/example/demo29/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo29;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class Controller {
+    @GetMapping("/Home")
+    public String api() {
+        return "Hello from Springboard";
+    }
+    
+
+}
